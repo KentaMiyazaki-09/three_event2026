ba35448053f9ec3803d7448faf030127ca2bb6b1
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2026-01-20 12:49:48 +0900

    エラーの解消と使っていない画像の削除

--- src/assets/js/_modules/_gradientCanvas.js
+++ src/assets/js/_modules/_gradientCanvas.js
@@ -1,7 +1,6 @@
 import { Gradient } from "./_lib/_Gradient";
 
 export default () => {
-  new Gradient().initGradient("#gradient-canvas-gray");
   new Gradient().initGradient("#gradient-canvas-RELAXING");
   new Gradient().initGradient("#gradient-canvas-NEUTRAL");
   new Gradient().initGradient("#gradient-canvas-INSPIRING");
